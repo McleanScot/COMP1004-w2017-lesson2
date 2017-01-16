c12ee61c6bfbb7bb68c645d96e9ab58c9ce3f319
Scot <scotmclean@live.com>
2017-01-16 14:46:10 -0500

    made some changes to the design of the calculator

---
+++ COMP1004-w2017-lesson2/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_w2017_lesson2
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculatorButton_Clicked(object sender, EventArgs e)
+        {
+            //could also do as Button
+            Button CalculatorButton = (Button)sender;
+
+            label1.Text += CalculatorButton.Text;
+        }
+    }
+}
